885200e23dce93d966d83224f4c4a6722b79dba3
Mazinflix <mazin@flixtechnology.com>
2022-04-18 13:29:14 +0200

    :lipstick: fixing why-us responsiveness

--- resources/views/cms/index.blade.php
+++ resources/views/cms/index.blade.php
@@ -483,7 +483,7 @@
     </section>
     <!-- blog section end -->
     
-    <section class="about-page section-b-space border-section ">
+    {{-- <section class="about-page section-b-space border-section ">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
@@ -557,6 +557,61 @@
                             </div>
                         </div>
                     </section>
+                </div>
+            </div>
+        </div>
+    </section> --}}
+    <section class="section-b-space beauty-about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="title1 section-t-space">
+                        @if (App::getLocale() == 'ar')
+                        <h2 class="title-inner1">لماذا نحن</h2>
+                        @else
+                        
+                        <h2 class="title-inner1">Why Us</h2>
+                        @endif
+                    </div>
+                </div>
+                <div class="col-xl-5 col-lg-6 col-md-12 offset-xl-1 text-center">
+                    @if (App::getlocale() == 'ar')
+                    <img src="/images/why-usar.png"alt="" class="img-fluid blur-up lazyload" srcset="" width="300" height="300">
+                        
+                    @else
+                    <img src="/images/why-us.png" alt="" class="img-fluid blur-up lazyload" srcset="" width="300" height="300">
+                        
+                    @endif
+                </div>
+                <div class="col-xl-5 col-lg-6 col-md-12">
+                    <div class="about-section">
+                        <div>
+                            <div class="about-text">
+                                @if (App::getLocale() == 'ar')
+                                <div class="col-sm-12">
+                                    <h2>لماذا عليكم اختيارنا؟</h2>
+                                    <h5>&#9832; حلقة الربط وجسر التواصل و التفاهم بين
+                                        العملاء وإستثمارتهم</h5>
+                                    <h5>&#9832; نقرب وجهات النظر بين المستثمرين</h5>
+                                    <h5>&#9832; نحن الحلقة التي تربط أطراف النجاح</h5>
+                                    <h5>&#9832; نشارك في تجاوز الصعوبات الإستثمارية</h5>
+                                    <h5>&#9832; نعمل على جعل حلم المستثمرين واقعا</h5>
+                                </div>
+                                    
+                                @else
+                                <div class="col-sm-12">
+                                    <h2>Why should you choose us?</h2>
+                                    <h5>&#9832; The linking ring and the bridge of communication and understanding between customers and their investments</h5>
+                                    <h5>&#9832; We bring the views of investors closer</h5>
+                                    <h5>&#9832; We are the link that connects the parties to success</h5>
+                                    <h5>&#9832; We participate in overcoming investment difficulties</h5>
+                                    <h5>&#9832; We are working to make the dream of investors a reality</h5>
+                                </div>
+                                    
+                                @endif
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
